216804993c1f560e07bb4ac191ffe23ff8bf5bf8
ananyabhtia <ananyabhtia@gmail.com>
2025-03-07 00:12:31 -0600

    app-styled

--- src/components/courselist.jsx
+++ src/components/courselist.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import '../components.css'
 
 const CourseList = ({courses}) => (
-    <div>
-        { Object.entries(courses).map(([key, entry]) => <h2 key={key}>{entry.term} CS {entry.number}: {entry.title}</h2>)}
+    <div className="courses-list">
+        { Object.entries(courses).map(([key, entry]) => 
+        <div className="courses-container">
+            <div className="code-name">
+                <h2 key={key} className='courses-code'>{entry.term} CS {entry.number}</h2>
+                <h3 key={key} className='courses-text'>{entry.title}</h3>
+            </div>
+            <div className="hr-meets">
+                <hr></hr>
+                <h3 key={key} className='courses-text'>{entry.meets}</h3>
+            </div>
+        </div>)}
     </div>
 );
 
